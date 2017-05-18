4cafce79904b859d2eb1d776d9797e02317625d7
jackHedaya <jackhedaya7@gmail.com>
2017-05-17 21:55:30 -0400

    completed full menu

--- sketch.js
+++ sketch.js
@@ -4,8 +4,10 @@ var account = {
   loggedIn: false,
   savePass: false,
 
-  username: "",
-  password: ""
+  username: "–",
+  password: "",
+  wins: "-",
+  losses: "-"
 }
 
 var register = {
@@ -181,6 +183,8 @@ async function setup() {
         account.loggedIn = true;
         account.username = userCookie;
         account.password = passCookie;
+        account.losses = snapshot.val().losses;
+        account.wins = snapshot.val().wins;
       }
       drawAll();
     });
@@ -198,39 +202,10 @@ async function mouseClicked() {
     return;
   }
 
-  if (mouseX > 10 && mouseX < 10 + tileSize / 2 && mouseY > 10 && mouseY < 10 + tileSize / 2) {
-    menu.opened = !menu.opened
-    if (menu.opened) {
-      for (var i = 0; i < 3 * (finalSizeW + 10 + tileSize / 2); i += 90) {
-        if (i + 90 >= 2 * (finalSizeW + 10 + tileSize / 2)) {
-          menu.incrUp = finalSizeW + 10 + tileSize / 2;
-        } else {
-          menu.incrUp = i / 3;
-        }
-        drawAll();
+  if (buttons.menu.isInBounds(mouseX, mouseY)) {
+    buttons.menu.handler();
 
-        await sleep(1);
-      }
-
-      delete menu.incrUp;
-      return;
-    } else {
-      for (var i = 3 * (finalSizeW + 10 + tileSize / 2); i > 0; i -= 90) {
-
-        if (i - 90 <= 0) {
-          menu.incrDown = 0;
-        } else {
-          menu.incrDown = i / 3;
-        }
-
-        drawAll();
-
-        await sleep(1);
-      }
-
-      delete menu.incrDown;
-      return;
-    }
+    drawAll();
   }
 
   if (!account.loggedIn) {
@@ -277,10 +252,18 @@ async function mouseClicked() {
       buttons.registerPasswordConf.handler();
 
       drawLogin();
+    } else if (buttons.joinMatch.isInBounds(mouseX, mouseY) && menu.opened) {
+      buttons.joinMatch.handler();
+
+      drawAll();
+    } else if (buttons.joinRandomMatch.isInBounds(mouseX, mouseY) && menu.opened) {
+      buttons.joinRandomMatch.handler();
+
+      drawAll();
     } else {
       firstHandler = null;
 
-      drawLogin();
+      drawAll();
     }
 
     if (buttons.registerButton.isInBounds(mouseX, mouseY) && !account.loggedIn && register.registerNow) {
@@ -288,6 +271,12 @@ async function mouseClicked() {
     }
 
     return;
+  } else {
+    if (buttons.logoutButton.isInBounds(mouseX, mouseY)) {
+      buttons.logoutButton.handler();
+
+      drawAll();
+    }
   }
 
   var pixelColor = {
@@ -298,6 +287,10 @@ async function mouseClicked() {
   }
 
   var position = null;
+
+  if (mouseX < finalSizeW || mouseX > finalSizeW + tileSize * 8) {
+    return;
+  }
 
   if (swPawn.sel) {
     if (compareColors(pixelColor.color, color(255, 255, 255)) && mouseY >= tileSize * 3) {
@@ -545,6 +538,46 @@ async function mouseClicked() {
   //writeBoard(board);
 }
 
+$(document).keypress(function(e) {
+  if (!firstHandler) {
+    return;
+  }
+
+  var character = String.fromCharCode(e.which);
+
+  if (e.which === 13) {
+    return;
+  }
+
+  // if (isNaN(character) && firstHandler === buttons.joinMatch) {
+  //   return;
+  // }
+
+  if (acceptableSize(buttons.loginUsername, 14) || acceptableSize(buttons.loginPassword, 14) || acceptableSize(buttons.registerUsername, 14) || acceptableSize(buttons.registerPassword, 14) || acceptableSize(buttons.registerPasswordConf, 14) || (acceptableSize(buttons.joinMatch, 12) && event.which != 8 && isNaN(String.fromCharCode(event.which)))) {
+    return;
+  }
+
+  if (character === " ") {
+    character = "_";
+  }
+
+  firstHandler.text += character;
+
+  drawAll();
+});
+
+$(document).keydown(function(e) {
+  if (!firstHandler) {
+    return;
+  }
+
+  if (e.which === 8) {
+    firstHandler.text = firstHandler.text.substr(0, firstHandler.text.length - 1);
+  }
+
+  drawAll();
+});
+
 function calcMov(start, end) {
 
   var changingY = (end.y - start.y) * tileSize;
@@ -762,7 +795,7 @@ function drawLogin() {
     buttons.loginPassword.draw();
 
     buttons.loginButton.textColor = color('white');
-    buttons.loginButton.buttonColor = color('#6F6DC5');
+    buttons.loginButton.buttonColor = color('#925BB5');
     buttons.loginButton.draw();
 
     var b = fonts.vera.textBounds("Don't have an account?", 0, 0, 12);
@@ -810,7 +843,7 @@ function drawLogin() {
     buttons.registerPasswordConf.passwordText = true;
     buttons.registerPasswordConf.draw();
 
-    buttons.registerButton.buttonColor = color('#6F6DC5');
+    buttons.registerButton.buttonColor = color('#925BB5');
     buttons.registerButton.textColor = color('white');
     buttons.registerButton.draw();
 
@@ -828,37 +861,6 @@ function drawLogin() {
     buttons.toLoginPage.draw();
   }
 }
-
-$(document).keypress(function(e) {
-  var character = String.fromCharCode(e.which);
-
-  if (e.which === 13) {
-    return;
-  }
-
-  if (acceptableSize(buttons.loginUsername, 14) || acceptableSize(buttons.loginPassword, 14) || acceptableSize(buttons.registerUsername, 14) || acceptableSize(buttons.registerPassword, 14) || acceptableSize(buttons.registerPasswordConf, 14)) {
-    return;
-  }
-
-  if (character === " ") {
-    character = "_";
-  }
-
-  firstHandler.text += character;
-  drawAll();
-});
-
-$(document).keydown(function(e) {
-  if (!firstHandler) {
-    return;
-  }
-
-  if (e.which === 8) {
-    firstHandler.text = firstHandler.text.substr(0, firstHandler.text.length - 1);
-  }
-
-  drawLogin();
-});
 
 function drawMenu() {
   if (menu.opened) {
@@ -882,14 +884,114 @@ function drawMenu() {
       image(images.x, 10, 10, tileSize / 2, tileSize / 2);
     }
   }
+
+  if (menu.opened && !menu.incrUp && !menu.incrDown && menu.incrUp !== 0 && !menu.incrDown !== 0) {
+    drawMenuPage();
+  }
+}
+
+function drawMenuPage() {
+  textFont(fonts.toThePoint);
+  textSize(30);
+  fill('white');
+  text("Join Match:", ((finalSizeW + 10 + tileSize / 2) - tileSize * 1.975) / 2, tileSize * 1.18);
+
+  buttons.joinMatch.draw();
+
+  buttons.joinRandomMatch.textColor = color('white');
+  buttons.joinRandomMatch.buttonColor = color('#333646');
+  buttons.joinRandomMatch.draw();
+
+  buttons.undoMove.textColor = color('white');
+  buttons.undoMove.buttonColor = color('#333646');
+  buttons.undoMove.draw();
+
+  textFont(fonts.toThePoint);
+  textSize(50);
+  text("Account: " + account.username, ((finalSizeW + 10 + tileSize / 2) - (fonts.toThePoint.textBounds("Account: " + account.username, 0, 0, 50).w)) / 2, tileSize * 3.4);
+
+  var string = "";
+  while (fonts.toThePoint.textBounds(string, 0, 0, 50).w < fonts.toThePoint.textBounds("Account: " + account.username, 0, 0, 50).w) {
+    string += "=";
+  }
+
+  text(string, ((finalSizeW + 10 + tileSize / 2) - (fonts.toThePoint.textBounds(string, 0, 0, 50).w)) / 2, tileSize * 3.7);
+
+  textSize(25);
+  textFont(fonts.adlanta);
+  text("Wins: " + account.wins, ((finalSizeW + 10 + tileSize / 2) - (fonts.adlanta.textBounds("Wins: " + account.wins, 0, 0, 25).w)) / 2, tileSize * 4);
+  text("Losses: " + account.losses, ((finalSizeW + 10 + tileSize / 2) - (fonts.adlanta.textBounds("Losses: " + account.losses, 0, 0, 25).w)) / 2, tileSize * 4.3);
+
+  buttons.changeUsername.buttonColor = color('#333646');
+  buttons.changeUsername.textColor = color('white');
+  buttons.changeUsername.draw();
+
+  buttons.changePassword.buttonColor = color('#333646');
+  buttons.changePassword.textColor = color('white');
+  buttons.changePassword.draw();
+
+  buttons.settings.buttonColor = color('#333646');
+  buttons.settings.textColor = color('white');
+  buttons.settings.draw();
+
+  buttons.logoutButton.buttonColor = color('#925BB5');
+  buttons.logoutButton.textColor = color('white');
+  buttons.logoutButton.draw();
+
+  buttons.contactUs.fontSize = 13.5;
+  buttons.contactUs.textColor = color('#FFA75D');
+  buttons.contactUs.draw();
+
+  buttons.reportBug.fontSize = 13.5;
+  buttons.reportBug.textColor = color('#FFA75D');
+  buttons.reportBug.draw();
 }
 
 async function setButtons() {
+  var menuSize = finalSizeW + 10 + tileSize / 2;
+
+  buttons.menu = new ImageButton(images.hamburgerBar, 10, 10, tileSize / 2, tileSize / 2, async function() {
+    menu.opened = !menu.opened
+    if (menu.opened) {
+      for (var i = 0; i < 3 * (finalSizeW + 10 + tileSize / 2); i += 90) {
+        if (i + 90 >= 2 * (finalSizeW + 10 + tileSize / 2)) {
+          menu.incrUp = finalSizeW + 10 + tileSize / 2;
+        } else {
+          menu.incrUp = i / 3;
+        }
+        drawAll();
+
+        await sleep(1);
+      }
+
+      delete menu.incrUp;
+      drawAll();
+      return;
+    } else {
+      for (var i = 3 * (finalSizeW + 10 + tileSize / 2); i > 0; i -= 90) {
+
+        if (i - 90 <= 0) {
+          menu.incrDown = 0;
+        } else {
+          menu.incrDown = i / 3;
+        }
+
+        drawAll();
+
+        await sleep(1);
+      }
+
+      delete menu.incrDown;
+      drawAll();
+      return;
+    }
+  });
+
   buttons.loginUsername = new TextField(finalSizeW + (2 * tileSize) + tileSize * (3 / 4), tileSize * 2.5 + tileSize / 4, tileSize * 2.5, 4 * (tileSize / 10));
   buttons.loginPassword = new TextField(finalSizeW + (2 * tileSize) + tileSize * (3 / 4), (tileSize * 2.5 + tileSize / 4) + 70, tileSize * 2.5, 4 * (tileSize / 10));
 
   buttons.staySignedIn = new CheckBox(finalSizeW + (2.2 * tileSize) + tileSize * (3 / 4), tileSize * 3.9 + tileSize / 4);
-  buttons.loginButton = new TextButton("Login", finalSizeW + (3.5 * tileSize), (tileSize * 2.5 + tileSize / 2.5) + (tileSize * 1.6), tileSize * 0.8, 4 * (tileSize / 10), function() {
+  buttons.loginButton = new TextButton("Log In", finalSizeW + (3.5 * tileSize), (tileSize * 2.5 + tileSize / 2.5) + (tileSize * 1.6), tileSize * 0.8, 4 * (tileSize / 10), function() {
     if (!buttons.loginUsername.text || !buttons.loginPassword.text) {
       return;
     }
@@ -900,6 +1002,8 @@ async function setButtons() {
         account.loggedIn = true;
         account.username = buttons.loginUsername.text;
         account.password = CryptoJS.AES.encrypt(buttons.loginPassword.text, "bohemian rhapsody");
+        account.wins = snapshot.val().wins;
+        account.losses = snapshot.val().losses;
         drawAll();
 
         if (buttons.staySignedIn.checked) {
@@ -915,6 +1019,9 @@ async function setButtons() {
 
         text("Invalid username or password", dhx, dhy);
       }
+
+      buttons.loginUsername.text = "";
+      buttons.loginPassword.text = "";
     });
   });
 
@@ -942,13 +1049,15 @@ async function setButtons() {
         }
       }
 
-      if (!(buttons.registerUsername.text.includes('.') && buttons.registerUsername.text.includes('$') && buttons.registerUsername.text.includes('[') && buttons.registerUsername.text.includes(']') && buttons.registerUsername.text.includes('#')) && buttons.registerUsername.text && buttons.registerPassword.text === buttons.registerPasswordConf && buttons.registerPassword.text.length > 6 && !alreadyFound) {
+      if (!(buttons.registerUsername.text.includes('.') && buttons.registerUsername.text.includes('$') && buttons.registerUsername.text.includes('[') && buttons.registerUsername.text.includes(']') && buttons.registerUsername.text.includes('#')) && buttons.registerUsername.text && buttons.registerPassword.text === buttons.registerPasswordConf.text && buttons.registerPassword.text.length > 6 && !alreadyFound) {
 
         var encrypted = CryptoJS.AES.encrypt(buttons.registerPassword.text, "bohemian rhapsody").toString();
 
         firebase.database().ref("accounts/" + buttons.registerUsername.text).set({
           name: buttons.registerUsername.text,
-          password: encrypted
+          password: encrypted,
+          wins: 0,
+          losses: 0
         });
 
         firstHandler = null;
@@ -969,7 +1078,7 @@ async function setButtons() {
           drawAll();
           fill('red');
           text("Username may not contain '.' '$' '[' ']' or '#'", finalSizeW + (2 * tileSize) + tileSize * (3 / 4), (tileSize * 2 + tileSize / 4) + 45);
-        } else if (buttons.registerPassword.text !== buttons.registerPasswordConf) {
+        } else if (buttons.registerPassword.text !== buttons.registerPasswordConf.text) {
           textFont(fonts.adlanta);
           textSize(11);
 
@@ -1031,6 +1140,43 @@ async function setButtons() {
     buttons.registerPassword.text = "";
     buttons.registerPasswordConf.text = "";
     firstHandler = null;
+  });
+
+  buttons.joinMatch = new TextField((menuSize - tileSize * 1.975) / 2, tileSize * 1.2, tileSize * 1.975, 4 * (tileSize / 10));
+  buttons.joinRandomMatch = new TextButton("Join Random Match", (menuSize - (fonts.vegur.textBounds("Join Random Match", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 1.8, fonts.vegur.textBounds("Join Random Match", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    print("JOIN!");
+  });
+  buttons.undoMove = new TextButton("Request Undo Move", (menuSize - (fonts.vegur.textBounds("Request Undo Move", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 2.4, fonts.vegur.textBounds("Request Undo Move", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    print("Coming Soon To Theaters!");
+
+    drawAll();
+  });
+  buttons.changeUsername = new TextButton("Change Username", (menuSize - (fonts.vegur.textBounds("Change Username", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 4.6, fonts.vegur.textBounds("Change Username", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    print("Almost there");
+  });
+  buttons.changePassword = new TextButton("Change Password", (menuSize - (fonts.vegur.textBounds("Change Password", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 5.2, fonts.vegur.textBounds("Change Password", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    print("Almost there");
+  });
+  buttons.settings = new TextButton("Settings", (menuSize - (fonts.vegur.textBounds("Settings", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 5.8, fonts.vegur.textBounds("Settings", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    print("Almost there");
+  });
+
+  buttons.logoutButton = new TextButton("Log Out", (menuSize - (fonts.vegur.textBounds("Log Out", 0, 0, 25).w + tileSize / 4)) / 2, tileSize * 6.9, fonts.vegur.textBounds("Log Out", 0, 0, 25).w + tileSize / 4, 4 * (tileSize / 10), function() {
+    account.loggedIn = false;
+    account.username = "-";
+    account.password = "";
+    account.losses = "-";
+    account.wins = "-";
+    account.savePass = false;
+    eraseCookie("username");
+    eraseCookie("password");
+  });
+
+  buttons.contactUs = new LabelButton("Contact Us", menuSize / 4, tileSize * 7.65, function() {
+    print("Available soon!");
+  });
+  buttons.reportBug = new LabelButton("Report Bug", 2 * (menuSize / 4), tileSize * 7.65, function() {
+    print("Available soon!");
   });
 }
 
